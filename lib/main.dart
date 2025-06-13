77bd4b1e4521fde57964ca2362fca4ca803c4b99
NabilRZ <nabilriano@gmail.com>
2025-06-13 12:53:04 +0700

    fixed loading screen

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,21 @@ import 'screens/menu_screen.dart';
 import 'screens/loading_screen.dart'; // Import the loading screen
 import 'models/address.dart';
 import 'models/menu_item.dart'; // Import MenuItem
+import 'models/user.dart'; // Import User model
+import 'models/cart_item.dart'; // Import CartItem model
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized(); // Ensure Flutter binding is initialized
   await Hive.initFlutter();
   Hive.registerAdapter(AddressAdapter());
   Hive.registerAdapter(MenuItemAdapter()); // Register MenuItemAdapter
+  Hive.registerAdapter(UserAdapter()); // Register UserAdapter
+  Hive.registerAdapter(CartItemAdapter()); // Register CartItemAdapter
   await Hive.openBox<Address>('addresses');
   await Hive.openBox<MenuItem>('favoriteMenus'); // Open a new box for favorite menus
+  await Hive.openBox<User>('userBox'); // Open a new box for user data
+  await Hive.openBox<int>('selectedAddressIndexBox'); // Open a new box for selected address index
+  await Hive.openBox<CartItem>('cartBox'); // Open a new box for cart items
   
   // Set system UI overlay style (optional)
   SystemChrome.setSystemUIOverlayStyle(
